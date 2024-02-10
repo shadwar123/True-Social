6cd1f1f98df4b94dea5c54632c49a571db383147
shadwar123 <shadwarnayyar@gmail.com>
2024-02-10 12:00:08 +0530

    Social Platform

---
+++ server/dbConnect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    const mongoUri =
+        "mongodb+srv://shadwar123:TxiJTYE00WXysSZ9@cluster0.yz6yzdu.mongodb.net/";
+
+    try {
+        mongoose.set('strictQuery', false);
+        const connect = await mongoose.connect(mongoUri, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+
+        console.log(`MongoDB connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
