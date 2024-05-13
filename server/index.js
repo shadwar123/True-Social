4221a64b3c68223d5ca943e3b50954d413213bf1
shadwar123 <shadwarnayyar@gmail.com>
2024-05-14 02:05:45 +0530

    depplo3

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(
     cors({
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
         origin: 'https://true-social-frontend.vercel.app',
     })
 );
